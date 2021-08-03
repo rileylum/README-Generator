4a6a2d6604b2ecf450aa588ea4581c335c60dc63
Riley Lum <rileylum@adam.com.au>
2021-08-03 12:02:03 +0930

    install inquirer, add basic prompt

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'name',
+        message: 'What is the project name?',
+        type: 'input'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => console.log(answers));
+}
 
 // Function call to initialize app
 init();
