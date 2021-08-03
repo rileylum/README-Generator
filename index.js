c2247640479ffeda05e1ecf75fbd1539332097cc
Riley Lum <rileylum@adam.com.au>
2021-08-03 13:15:22 +0930

    add markdown boiler plate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -63,7 +64,7 @@ function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => console.log(answers));
+    inquirer.prompt(questions).then((answers) => console.log(generateMarkdown(answers)));
 }
 
 // Function call to initialize app
