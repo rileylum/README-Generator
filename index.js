522cee020e122c3d9ffdf85528e2f76fb5642065
Riley Lum <rileylum@adam.com.au>
2021-08-03 13:34:22 +0930

    add write file function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
@@ -60,11 +61,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('success')
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => console.log(generateMarkdown(answers)));
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('README.md', generateMarkdown(answers));
+    });
 }
 
 // Function call to initialize app
