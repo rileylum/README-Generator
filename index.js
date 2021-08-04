48a6419d93009b6c4b61834dbe02c34646accddf
Riley Lum <rileylum@adam.com.au>
2021-08-05 09:08:56 +0930

    add license information to function

--- index.js
+++ index.js
@@ -7,38 +7,23 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        name: 'fullName',
+        message: 'What is your full name?',
+        type: 'input'
+    },
+    {
         name: 'title',
         message: 'What is the project title?',
         type: 'input'
     },
     {
-        name: 'descMotivation',
-        message: 'What is the motivation behind the project?',
-        type: 'input'
-    },
-    {
-        name: 'descWhy',
-        message: 'Why did you build this project?',
-        type: 'input'
-    },
-    {
-        name: 'descProblem',
-        message: 'What problem does the project solve?',
-        type: 'input'
-    },
-    {
-        name: 'descLearn',
-        message: 'What did you learn building the project?',
+        name: 'description',
+        message: 'Please provide a description for your project.',
         type: 'input'
     },
     {
         name: 'install',
         message: 'How do you install this project?',
-        type: 'input'
-    },
-    {
-        name: 'test',
-        message: 'How do you test this project?',
         type: 'input'
     },
     {
@@ -52,10 +37,25 @@ const questions = [
         type: 'input'
     },
     {
-        name: 'licencse',
+        name: 'test',
+        message: 'How do you test this project?',
+        type: 'input'
+    },
+    {
+        name: 'github',
+        message: 'What is your github username?',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'What is your email?',
+        type: 'input'
+    },
+    {
+        name: 'license',
         message: 'What license is this project being released under?',
         type: 'list',
-        choices: ['MIT', 'another one']
+        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT Licence', 'ISC Licence']
     }
 
 ];
